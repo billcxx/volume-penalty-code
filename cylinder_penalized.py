a836ce9fcea977142e44f922299d3755816136b7
Xiaoxing Cheng <hello@chengxiaoxing.me>
2024-04-18 23:26:34 +0800

    use ascii

--- cylinder_penalized.py
+++ cylinder_penalized.py
@@ -14,12 +14,12 @@ logger = logging.getLogger(__name__)
 
 # Simulation parameters
 Re = 100
-η = 1e-2
-ε = np.sqrt(η/Re)
-δ = 3.8017192844*ε
+eta = 1e-2
+epsilon = np.sqrt(eta/Re)
+delta = 3.8017192844*epsilon
 l = 0
 N = [256,64,64,128]
-boundaries = sorted(set([.1,1-l-δ,1-l+δ,4]))
+boundaries = sorted(set([.1,1-l-delta,1-l+delta,4]))
 iterations, wall_time = 1000+1, 1*60*60
 dt = 5e-3
 print_freq = 10
@@ -28,12 +28,12 @@ data_dir = os.path.join('runs',sim_name)
 if rank==0 and not os.path.isdir(data_dir): os.makedirs(data_dir)
 
 # Create problem bases and domain
-θbasis = de.Fourier('θ', N[0], interval=(-np.pi,np.pi), dealias=3/2)
+thetabasis = de.Fourier('theta', N[0], interval=(-np.pi,np.pi), dealias=3/2)
 rbases = [de.Chebyshev(f'r{i}',N[i+1],interval=boundaries[i:i+2], dealias=3/2) for i in range(len(boundaries)-1)]
 rbasis = de.Compound('r',rbases)
-domain = de.Domain([θbasis,rbasis], grid_dtype=np.float64)
-θ, r = domain.grids(domain.dealias)
-θθ,rr = np.meshgrid(θ,r,indexing='ij')
+domain = de.Domain([thetabasis,rbasis], grid_dtype=np.float64)
+theta, r = domain.grids(domain.dealias)
+theta2,rr = np.meshgrid(theta,r,indexing='ij')
 
 # Boundary condition functions
 from dedalus.core.operators import GeneralFunction
@@ -56,42 +56,42 @@ bc = ConstantFunction(domain, layout='g', func=bc_func)
 oscillation = ConstantFunction(domain, layout='g', func=oscillation_func)
 
 # Volume penalty mask
-Γ = domain.new_field(scales=domain.dealias)
-Γ['g'] = normalized_mask((rr-1)/δ)
+gamma = domain.new_field(scales=domain.dealias)
+gamma['g'] = normalized_mask((rr-1)/delta)
 
 disk = de.IVP(domain, variables=['u','v','p','q'], ncc_cutoff=1e-10)
 disk.meta[:]['r']['dirichlet'] = True
 
 # Parameters
-params = [boundaries[0],boundaries[-1],Re,bc,np.pi] + N + [η, ε, δ, l, Γ]
-param_names = ['R0','R1','Re','bc','π','Nθ']+[f'Nr{i}' for i in range(len(N)-1)] +['η', 'ε', 'δ', 'l', 'Γ']
+params = [boundaries[0],boundaries[-1],Re,bc,np.pi] + N + [eta, epsilon, delta, l, gamma]
+param_names = ['R0','R1','Re','bc','PI','Ntheta']+[f'Nr{i}' for i in range(len(N)-1)] +['eta', 'epsilon', 'delta', 'l', 'gamma']
 if len(params)==len(param_names):
     for param, param_name in zip(params, param_names): 
         disk.parameters[param_name] = param
 
 disk.substitutions['pr'] = "p - 0.5*(u*u+v*v)"
 disk.substitutions['qr'] = "q/r"
-disk.substitutions['c'] = "cos(θ)"
-disk.substitutions['s'] = "sin(θ)"
+disk.substitutions['c'] = "cos(theta)"
+disk.substitutions['s'] = "sin(theta)"
 disk.substitutions['fpr'] = "-pr"
-disk.substitutions['fpθ'] = "0"
+disk.substitutions['fptheta'] = "0"
 disk.substitutions['fvr'] = "0"
-disk.substitutions['fvθ'] = "(dθ(u) + r*dr(v) - v)/(Re*r)"
-disk.substitutions['φ'] = "(π/2)*(1-cos(2*t/π))"
-disk.substitutions['ω'] = "sin(2*t/π)"
-disk.substitutions['α'] = "(2/π)*cos(2*t/π)"
+disk.substitutions['fvtheta'] = "(dtheta(u) + r*dr(v) - v)/(Re*r)"
+disk.substitutions['phi'] = "(PI/2)*(1-cos(2*t/PI))"
+disk.substitutions['omega'] = "sin(2*t/PI)"
+disk.substitutions['alpha'] = "(2/PI)*cos(2*t/PI)"
 
-disk.add_equation("dr(r*u) + dθ(v) = 0")
-disk.add_equation("r*r*dt(u) + (1/Re)*dθ(q) +       r*r*dr(p) =  r*v*q - (r*r*Γ/η)*u")
-disk.add_equation("r*r*dt(v) - (1/Re)*(r*dr(q) - q) + r*dθ(p) = -r*u*q - (r*r*Γ/η)*(v-r*ω)")
-disk.add_equation("q - dr(r*v) + dθ(u) = 0")
+disk.add_equation("dr(r*u) + dtheta(v) = 0")
+disk.add_equation("r*r*dt(u) + (1/Re)*dtheta(q) +       r*r*dr(p) =  r*v*q - (r*r*gamma/eta)*u")
+disk.add_equation("r*r*dt(v) - (1/Re)*(r*dr(q) - q) + r*dtheta(p) = -r*u*q - (r*r*gamma/eta)*(v-r*omega)")
+disk.add_equation("q - dr(r*v) + dtheta(u) = 0")
 
 # Boundary conditions                                                       
 disk.add_bc("left(u)  = 0")
-disk.add_bc("left(v)  = ω*R0")
-disk.add_bc("right(u) = bc*cos(θ)", condition="(nθ != 0)")
-disk.add_bc("right(v) =-bc*sin(θ)")
-disk.add_bc("right(p) = 0", condition="(nθ == 0)")
+disk.add_bc("left(v)  = omega*R0")
+disk.add_bc("right(u) = bc*cos(theta)", condition="(ntheta != 0)")
+disk.add_bc("right(v) =-bc*sin(theta)")
+disk.add_bc("right(p) = 0", condition="(ntheta == 0)")
 
 # Build timestepper and solver
 ts = de.timesteppers.SBDF3
@@ -109,22 +109,22 @@ for field in [u,v,p,q]:
 # Save state variables
 analysis = solver.evaluator.add_file_handler('{}/data-{}'.format(data_dir,sim_name), iter=10, max_writes=200,mode='overwrite')
 for task in disk.variables: analysis.add_task(task)
-analysis.add_task("ω")
-analysis.add_task("φ")
-analysis.add_task("α")
+analysis.add_task("omega")
+analysis.add_task("phi")
+analysis.add_task("alpha")
 analysis.add_task("bc")
 
 # Save force calcs
 forces = solver.evaluator.add_file_handler('{}/force-{}'.format(data_dir,sim_name), 
                                            iter=1, max_writes=iterations,mode='overwrite')
-forces.add_task("integ(interp((c*fpr-s*fpθ)*r,r='left'),'θ')",name='Fpx')
-forces.add_task("integ(interp((c*fvr-s*fvθ)*r,r='left'),'θ')",name='Fvx')
-forces.add_task("integ(interp((s*fpr+c*fpθ)*r,r='left'),'θ')",name='Fpy')
-forces.add_task("integ(interp((s*fvr+c*fvθ)*r,r='left'),'θ')",name='Fvy')
-forces.add_task("integ(interp(fvθ*r*r,r='left'),'θ')",name='Tv')
-forces.add_task("ω")
-forces.add_task("φ")
-forces.add_task("α")
+forces.add_task("integ(interp((c*fpr-s*fptheta)*r,r='left'),'theta')",name='Fpx')
+forces.add_task("integ(interp((c*fvr-s*fvtheta)*r,r='left'),'theta')",name='Fvx')
+forces.add_task("integ(interp((s*fpr+c*fptheta)*r,r='left'),'theta')",name='Fpy')
+forces.add_task("integ(interp((s*fvr+c*fvtheta)*r,r='left'),'theta')",name='Fvy')
+forces.add_task("integ(interp(fvtheta*r*r,r='left'),'theta')",name='Tv')
+forces.add_task("omega")
+forces.add_task("phi")
+forces.add_task("alpha")
 
 # Save simulation parameters   
 parameters = solver.evaluator.add_file_handler('{}/parameters-{}'.format(data_dir,sim_name), iter=np.inf, max_writes=np.inf,mode='overwrite')
